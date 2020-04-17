3497ecff07974b7f478fb6071fc6c3799172c0ad
Taufik <taufik.algi99@gmail.com>
2020-04-17 10:59:28 +0700

    feat: done implement all features

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import HomePage from "./views/HomePage";
+import CreatePostPage from "./views/CreatePostPage";
+import SinglePostPage from "./views/SinglePostPage";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/post/:id" component={SinglePostPage} />
+          <Route path="/create" component={CreatePostPage} />
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/views/SinglePostPage.js
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+
+function SinglePostPage(props) {
+  const [post, setPost] = useState({});
+
+  const postID = props.match.params.id;
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8000/api/posts/get/${postID}`)
+      .then((response) => setPost(response.data.post))
+      .catch((error) => console.log(error));
+  }, [postID]);
+
+  return (
+    <Container>
+      <h1>{post.title}</h1>
+      <p>{post.body}</p>
+      <p>Author: {post.author}</p>
+    </Container>
+  );
+}
+
+export default SinglePostPage;
